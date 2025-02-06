509da51a67deaa21bf07b303edf5442ad95c718e
Maria A. Valencia <fafevalo99@gmail.com>
2025-02-05 19:46:51 -0500

    j

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaDeAmigos = [];
-let nuevoAmigo = "k";
+let nuevoAmigo;
 
 function asignarTextoElemento(elemento, texto) {
     let elementoHtml = document.querySelector(elemento);
@@ -13,23 +13,26 @@ function asignarTextoElemento(elemento, texto) {
 function agregarAmigo(){
     nuevoAmigo = document.getElementById('amigo').value;
     console.log(nuevoAmigo);
-    if (nuevoAmigo == "" || isNaN(nuevoAmigo)){
+
+    if ( nuevoAmigo !== ""){
+        if (nuevoAmigo.in(listaDeAmigos)){
+            return asignarTextoElemento("#resultado", " 🚫 Este nombre ya esta en la lista, ingrese uno nuevo")
+          
+    } else {
+    
         asignarTextoElemento("#resultado", "❌ Ingrese un nombre primero");
         setTimeout(() =>{ 
             asignarTextoElemento("#resultado", "");
         }, 2000);
-    } else {
-        if (nuevoAmigo.includes(listaDeAmigos)){
-          return asignarTextoElemento("#resultado", " 🚫 Este nombre ya esta en la lista")
-        }else {
-             listaDeAmigos.push(nuevoAmigo);
-             asignarTextoElemento("#listaAmigos", `${listaDeAmigos}`);
-             return nuevoAmigo;
-             console.log(listaDeAmigos)
-        }
- 
-        
     }
+    // if (nuevoAmigo === "" || isNaN(nuevoAmigo)){
+        
+        
+    //     c
+       
     
 }
 
+        listaDeAmigos.push(nuevoAmigo);
+        asignarTextoElemento("#listaAmigos", `${listaDeAmigos}`);
+        console.log(listaDeAmigos);
